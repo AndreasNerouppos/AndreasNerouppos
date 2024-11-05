53902d29e60d16aa5a67769877d1367f0158e653
AndreasNerouppos <andreasnerrou@gmail.com>
2024-11-05 18:42:39 +0200

    trolley

---
+++ trolley/src/trolley.c
@@ -0,0 +1,29 @@
+//Εργασία 0 Η Άσκηση με το trolley
+
+#include<stdio.h>
+
+int main(){  //Συνάρτηση main
+long int k,left, right;  // Δήλωση μεταβλητών
+
+while(1){
+//Έλεγχος για το πρώτο κόστος
+		printf("Please enter the cost of going left: \n");
+	if((k=scanf("%ld",&left)) !=1 || k==EOF){
+		printf("Terminating.\n");
+return 0;	
+	}		
+//Έλεγχος για το δεύτερο κόστος
+        	printf("Please enter the cost of going right: \n");
+	if(scanf("%ld",&right) !=1 ){
+                printf("No right cost provided. \n");
+return 1;
+        }
+
+// Τυπώνω την απόφαση με το μικρότερο κόστος
+	if(left<=right)
+                printf("Go left. \n");
+        else
+                printf("Go right.\n");
+}
+	}
+                                  
