89ec10103128162ac194496085b68e37febc0ab7
AndreasNerouppos <andreasnerrou@gmail.com>
2024-11-10 13:06:31 +0200

    trolley

--- trolley/src/trolley.c
+++ trolley/src/trolley.c
@@ -7,21 +7,21 @@ long int l,r,left, right;  // Δήλωση μεταβλητών
 
 while(1){
 //Έλεγχος για το πρώτο κόστος
-		printf("Please enter the cost of going left: \n");
+		printf("Please enter the cost of going left:\n");
 	if((l=scanf("%ld",&left)) !=1 || l==EOF){
 		printf("Terminating.\n");
 return 0;	
 	}		
 //Έλεγχος για το δεύτερο κόστος
-        	printf("Please enter the cost of going right: \n");
+        	printf("Please enter the cost of going right:\n");
 	if((r=scanf("%ld",&right)) !=1 || r==EOF ){
-                printf("No right cost provided. \n");
+                printf("No right cost provided.\n");
 return 1;
         }
 
 // Τυπώνω την απόφαση με το μικρότερο κόστος
 	if(left<=right)
-                printf("Go left. \n");
+                printf("Go left.\n");
         else
                 printf("Go right.\n");
 }
